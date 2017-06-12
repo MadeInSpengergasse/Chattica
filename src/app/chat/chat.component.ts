95a699f0bff9a4132b2165230058bae6ac4870a2
Luca Weiss <luca@z3ntu.xyz>
2017-06-12 09:00:54 +0200

    WHY DID I DO THIS?????

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AfterViewChecked, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {ChatService} from '../services/chat.service';
 import {Subscription} from 'rxjs/Subscription';
 
@@ -7,7 +7,8 @@ import {Subscription} from 'rxjs/Subscription';
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit, OnDestroy {
+export class ChatComponent implements OnInit, OnDestroy, AfterViewChecked {
+  @ViewChild('scrollMe') private myScrollContainer: ElementRef;
   currentMessage: string;
   private _subscription: Subscription;
 
@@ -18,6 +19,7 @@ export class ChatComponent implements OnInit, OnDestroy {
   ngOnInit() {
     console.log('ngOnInit()');
     this._subscription = this._chatService.getDataStream().subscribe();
+    this.scrollToBottom();
   }
 
   ngOnDestroy(): void {
@@ -37,4 +39,21 @@ export class ChatComponent implements OnInit, OnDestroy {
   getChatMessages() {
     return this._chatService.chatMessages;
   }
+
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom(): void {
+    console.log('SCROLL NOW!');
+    console.log(this.myScrollContainer.nativeElement.scrollHeight);
+    console.log(this.myScrollContainer.nativeElement.scrollTop);
+    try {
+      // this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+      this.myScrollContainer.nativeElement.scrollTop = 1500;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
 }
